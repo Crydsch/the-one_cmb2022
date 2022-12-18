43162aeb4cefd5da4415bec61fc1aa9976d338ee
Justus von der Beek <beek@in.tum.de>
2022-12-18 16:31:07 +0100

    Updated active evaluation

--- src/movement/map/TimetableNode.java
+++ src/movement/map/TimetableNode.java
@@ -2,40 +2,36 @@ package movement.map;
 
 import core.Coord;
 import core.Settings;
+import movement.TimetableMovement;
 
 public class TimetableNode {
-
-    public static final String MAP_BASE_MOVEMENT_NS = "TimetableMovement";
-    public static final String SEC_PER_ITER = "secondsPer1Iter";
-    public static final String START_DAY_TIME = "startOfDay";
-    public static final String DEF_DUR = "defActivityDur";
 
     // Adding the time information to this node
     private double startTime;
     private double endTime;
+    private double stepsPerHour;
     private MapNode node;
 
-    public TimetableNode(MapNode location, double startTime) {
+    public TimetableNode(MapNode location, double startTime, double stepsPerHour) {
         this.node = location;
         this.startTime = startTime;
         this.endTime = -1.0;
+        this.stepsPerHour = stepsPerHour;
     }
 
     /** Returns 1 if the activity is yet to come.
      * Returns 0 if the activity is starting exactly now
      * Returns -1 if the activty is already due
      */
-    public int canExecute(double currentTime) {
-        Settings settings = new Settings(MAP_BASE_MOVEMENT_NS);
-        double secPerIter = settings.getDouble(SEC_PER_ITER);
-        int startOfDay = settings.getInt(START_DAY_TIME);
-        endTime = startTime + settings.getDouble(DEF_DUR);
+    public int canExecute(double currentIteration) {
+        Settings settings = new Settings(TimetableMovement.TIMETABLE_MOVEMENT_NS);
+        int startOfDay = settings.getInt(TimetableMovement.START_DAY_TIME);
+        endTime = startTime + (settings.getDouble(TimetableMovement.DEF_DUR) * stepsPerHour);
 
         // Time in hours
-        double time = (double)startOfDay + ((currentTime * secPerIter) / (60 * 60));
-        if (time < startTime)
+        if (currentIteration < startTime)
             return 1;
-        if (startTime < time && time < endTime)
+        if (startTime < currentIteration && currentIteration < endTime)
             return 0;
         // Else case
         return -1;
